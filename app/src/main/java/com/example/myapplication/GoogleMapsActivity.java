442a018b8671ddbefa919dcb8d24c0510b015284
Niru-Prajapati <prajneeru@gmail.com>
2026-02-11 11:58:46 +0545

    added API

---
+++ app/src/main/java/com/example/myapplication/GoogleMapsActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.libraries.maps.CameraUpdateFactory;
+import com.google.android.libraries.maps.GoogleMap;
+import com.google.android.libraries.maps.OnMapReadyCallback;
+import com.google.android.libraries.maps.SupportMapFragment;
+import com.google.android.libraries.maps.model.LatLng;
+import com.google.android.libraries.maps.model.MarkerOptions;
+
+public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_google_maps);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+        //Adding latitude and longitude
+        LatLng location = new LatLng(26,100);
+
+        //Adding red marker to point location
+        mMap.addMarker(new MarkerOptions().position(location).title("Unknown"));
+
+        //Moving camera to desired location
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+        //Adding zoom effect
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10.0f));
+    }
+
+}
